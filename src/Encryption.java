832497773a317c57436fdf06a41ba89022c72e66
ccw630 <ccw630@gmail.com>
2019-07-02 01:42:53 +0800

    initial commit

---
+++ src/Encryption.java
@@ -0,0 +1,127 @@
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.RandomAccessFile;
+import java.security.AlgorithmParameters;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+
+public class Encryption {
+    private File file;
+    private String password;
+
+    Encryption(File file, String password) {
+        this.file = file;
+        this.password = password;
+    }
+
+    public boolean encrypt() throws Exception {
+        if (!file.exists() || password == null || file == null) {
+            return false;
+        }
+
+        byte[] salt = new byte[8];
+        SecureRandom srand = new SecureRandom();
+        srand.nextBytes(salt);
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, genKey(password.toCharArray(), salt));
+        AlgorithmParameters params = cipher.getParameters();
+
+        byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
+
+        FileInputStream in = new FileInputStream(file);
+        FileOutputStream out = preOutput(in, file.getParent() +
+                "/" + file.getName() + ".tmp", cipher);
+        byte[] output = cipher.doFinal();
+        if (output != null) {
+            out.write(output);
+        }
+
+        in.close();
+        out.flush();
+        out.write(salt);
+        out.write(iv);
+        out.close();
+
+        File temp = new File(file.getParent() + "/" + file.getName() + ".tmp");
+        file.delete();
+        return temp.renameTo(file);
+    }
+
+    public boolean decrypt() throws Exception {
+        if (!file.exists() || password == null || file == null) {
+            return false;
+        }
+
+        RandomAccessFile raf = new RandomAccessFile(file, "rw");
+        byte[] salt = new byte[8];
+        byte[] iv = new byte[16];
+        raf.seek(file.length() - (salt.length + iv.length));
+        raf.read(salt, 0, salt.length);
+        raf.seek(file.length() - (iv.length));
+        raf.read(iv, 0, iv.length);
+        raf.setLength(file.length() - (salt.length + iv.length));
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, genKey(password.toCharArray(), salt), new IvParameterSpec(iv));
+
+        FileInputStream in = new FileInputStream(file);
+        FileOutputStream out = preOutput(in, file.getParent() +
+                "/" + file.getName() + ".tmp", cipher);
+
+        try {
+            byte[] output = cipher.doFinal();
+            if (output != null) {
+                out.write(output);
+            }
+        } catch (Exception e) {
+            raf.seek(file.length());
+            raf.write(salt);
+            raf.seek(file.length());
+            raf.write(iv);
+            raf.close();
+            in.close();
+            out.close();
+            File temp = new File(file.getParent() + "/" + file.getName() + ".tmp");
+            temp.delete();
+            return false;
+        }
+
+        raf.close();
+        in.close();
+        out.flush();
+        out.close();
+
+        File temp = new File(file.getParent() + "/" + file.getName() + ".tmp");
+        file.delete();
+        temp.renameTo(file);
+        return true;
+    }
+
+    private SecretKey genKey(char[] password, byte[] salt) throws Exception {
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        KeySpec keySpec = new PBEKeySpec(password, salt, 65536, 256);
+        SecretKey tmp = factory.generateSecret(keySpec);
+        return new SecretKeySpec(tmp.getEncoded(), "AES");
+    }
+
+    private FileOutputStream preOutput(FileInputStream in, String path, Cipher cipher) throws Exception {
+        FileOutputStream out = new FileOutputStream(path);
+        byte[] b = new byte[64];
+        int read;
+        while ((read = in.read(b)) != -1) {
+            byte[] output = cipher.update(b, 0, read);
+            if (output != null) {
+                out.write(output);
+            }
+        }
+        return out;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,104 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Main extends Application {
+    private File file = null;
+
+    static final String NOFILE = "文件不能为空\n ";
+    static final String NOPASS = "口令不能为空\n ";
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("请选择文件");
+
+        Button browseButton = new Button("浏览");
+        Button encryptButton = new Button("加密");
+        Button decryptButton = new Button("解密");
+
+        Label fileLabel = new Label("文件");
+        Label passLabel = new Label("口令");
+        Label infoLabel = new Label("请选择要加/解密的文件");
+
+        TextField passTextField = new TextField();
+        TextField fileTextField = new TextField();
+        fileTextField.setDisable(true);
+
+        GridPane gridPane = new GridPane();
+
+        gridPane.add(fileLabel, 0, 0);
+        gridPane.add(fileTextField, 1, 0);
+        gridPane.add(browseButton, 2, 0);
+        gridPane.add(passLabel, 0, 1);
+        gridPane.add(passTextField, 1, 1);
+        gridPane.add(infoLabel, 0, 2, 3, 1);
+        gridPane.add(encryptButton, 0, 3);
+        gridPane.add(decryptButton, 1, 3);
+
+        browseButton.setOnAction(e -> {
+            file = fc.showOpenDialog(stage);
+            if (file == null) {
+                fileTextField.setText(null);
+            } else {
+                fileTextField.setText(file.getName());
+                infoLabel.setText("请输入口令");
+            }
+        });
+
+        encryptButton.setOnAction(e -> {
+            if (file == null || !file.exists()) {
+                infoLabel.setText(NOFILE);
+            } else if (passTextField.getText().equals("")) {
+                infoLabel.setText(NOPASS);
+            } else {
+                Encryption Encryption = new Encryption(file, passTextField.getText());
+                try {
+                    if (Encryption.encrypt()) {
+                        infoLabel.setText("加密成功");
+                    } else {
+                        infoLabel.setText("加密失败，请检查文件是否有误 ");
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        decryptButton.setOnAction(e -> {
+            if (file == null || !file.exists()) {
+                infoLabel.setText(NOFILE);
+            } else if (passTextField.getText().equals("")) {
+                infoLabel.setText(NOPASS);
+            } else {
+                Encryption Encryption = new Encryption(file, passTextField.getText());
+                try {
+                    if (Encryption.decrypt()) {
+                        infoLabel.setText("解密成功");
+                    } else {
+                        infoLabel.setText("解密失败，请检查口令或文件是否正确\n ");
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        Scene scene = new Scene(gridPane);
+        stage.setTitle("文件加密管理器");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String args[]) {
+        launch(args);
+    }
+}
